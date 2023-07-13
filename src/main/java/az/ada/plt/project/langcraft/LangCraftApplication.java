252d65336c0ca56226b7da3db0fe9c4dae178f85
ngasimzada <nabat.gasimzade@pashabank.az>
2023-07-13 23:51:58 +0400

    Initial commit

---
+++ src/main/java/az/ada/plt/project/langcraft/LangCraftApplication.java
@@ -0,0 +1,13 @@
+package az.ada.plt.project.langcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangCraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ada/plt/project/langcraft/LangCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ada.plt.project.langcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
